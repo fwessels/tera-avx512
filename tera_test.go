d447191c0ffa20f3022a797668c7c64012001a7a
Frank Wessels <fwessels@xs4all.nl>
2017-11-16 18:54:25 +0000

    Add multithreaded benchmarks

--- tera_test.go
+++ tera_test.go
@@ -5,27 +5,31 @@ import (
 	"testing"
 )
 
-func benchmarkMullW(b *testing.B, size int) {
+func benchmarkMullW(b *testing.B, cores int) {
 
-	var wg sync.WaitGroup
+	if cores == 1 {
+		for i := 0; i < b.N; i++ {
+			teraAVX512()
+		}
+	} else {
 
-	b.SetBytes(int64(size))
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		wg.Add(4)
-		go func() { teraAVX512(); wg.Done() }()
-		go func() { teraAVX512(); wg.Done() }()
-		go func() { teraAVX512(); wg.Done() }()
-		go func() { teraAVX512(); wg.Done() }()
-		// go func() { teraAVX512(); wg.Done() }()
-		// go func() { teraAVX512(); wg.Done() }()
-		// go func() { teraAVX512(); wg.Done() }()
-		// go func() { teraAVX512(); wg.Done() }()
-		wg.Wait()
-		//teraAVX512()
+		var wg sync.WaitGroup
+
+		for i := 0; i < b.N; i++ {
+			wg.Add(cores)
+			for c := 0; c < cores; c++ {
+				go func() { teraAVX512(); wg.Done() }()
+			}
+			wg.Wait()
+		}
 	}
 }
 
-func BenchmarkMullW(b *testing.B) {
-	benchmarkMullW(b, 128*1024)
-}
+func BenchmarkMullW(b *testing.B)        { benchmarkMullW(b, 1) }
+func BenchmarkMullW_2Core(b *testing.B)  { benchmarkMullW(b, 2) }
+func BenchmarkMullW_4Core(b *testing.B)  { benchmarkMullW(b, 4) }
+func BenchmarkMullW_8Core(b *testing.B)  { benchmarkMullW(b, 8) }
+func BenchmarkMullW_12Core(b *testing.B) { benchmarkMullW(b, 12) }
+func BenchmarkMullW_16Core(b *testing.B) { benchmarkMullW(b, 16) }
+func BenchmarkMullW_24Core(b *testing.B) { benchmarkMullW(b, 24) }
+func BenchmarkMullW_32Core(b *testing.B) { benchmarkMullW(b, 32) }
