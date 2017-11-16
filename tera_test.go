054a3284d1b312f4e2e57684b4d6b65414180bea
Frank Wessels <fwessels@xs4all.nl>
2017-11-16 19:34:54 +0000

    Added DOUBLE/int32 benchmark

--- tera_test.go
+++ tera_test.go
@@ -5,11 +5,11 @@ import (
 	"testing"
 )
 
-func benchmarkMullW(b *testing.B, cores int) {
+func benchmarkMullW(b *testing.B, cores int, f func()) {
 
 	if cores == 1 {
 		for i := 0; i < b.N; i++ {
-			teraAvx512Int16()
+			f()
 		}
 	} else {
 
@@ -18,18 +18,27 @@ func benchmarkMullW(b *testing.B, cores int) {
 		for i := 0; i < b.N; i++ {
 			wg.Add(cores)
 			for c := 0; c < cores; c++ {
-				go func() { teraAvx512Int16(); wg.Done() }()
+				go func() { f(); wg.Done() }()
 			}
 			wg.Wait()
 		}
 	}
 }
 
-func BenchmarkMullW(b *testing.B)        { benchmarkMullW(b, 1) }
-func BenchmarkMullW_2Core(b *testing.B)  { benchmarkMullW(b, 2) }
-func BenchmarkMullW_4Core(b *testing.B)  { benchmarkMullW(b, 4) }
-func BenchmarkMullW_8Core(b *testing.B)  { benchmarkMullW(b, 8) }
-func BenchmarkMullW_12Core(b *testing.B) { benchmarkMullW(b, 12) }
-func BenchmarkMullW_16Core(b *testing.B) { benchmarkMullW(b, 16) }
-func BenchmarkMullW_24Core(b *testing.B) { benchmarkMullW(b, 24) }
-func BenchmarkMullW_32Core(b *testing.B) { benchmarkMullW(b, 32) }
+func BenchmarkMulInt16(b *testing.B)        { benchmarkMullW(b, 1, teraAvx512Int16) }
+func BenchmarkMulInt16_2Core(b *testing.B)  { benchmarkMullW(b, 2, teraAvx512Int16) }
+func BenchmarkMulInt16_4Core(b *testing.B)  { benchmarkMullW(b, 4, teraAvx512Int16) }
+func BenchmarkMulInt16_8Core(b *testing.B)  { benchmarkMullW(b, 8, teraAvx512Int16) }
+func BenchmarkMulInt16_12Core(b *testing.B) { benchmarkMullW(b, 12, teraAvx512Int16) }
+func BenchmarkMulInt16_16Core(b *testing.B) { benchmarkMullW(b, 16, teraAvx512Int16) }
+func BenchmarkMulInt16_24Core(b *testing.B) { benchmarkMullW(b, 24, teraAvx512Int16) }
+func BenchmarkMulInt16_32Core(b *testing.B) { benchmarkMullW(b, 32, teraAvx512Int16) }
+
+func BenchmarkMulInt32(b *testing.B)        { benchmarkMullW(b, 1, teraAvx512Int32) }
+func BenchmarkMulInt32_2Core(b *testing.B)  { benchmarkMullW(b, 2, teraAvx512Int32) }
+func BenchmarkMulInt32_4Core(b *testing.B)  { benchmarkMullW(b, 4, teraAvx512Int32) }
+func BenchmarkMulInt32_8Core(b *testing.B)  { benchmarkMullW(b, 8, teraAvx512Int32) }
+func BenchmarkMulInt32_12Core(b *testing.B) { benchmarkMullW(b, 12, teraAvx512Int32) }
+func BenchmarkMulInt32_16Core(b *testing.B) { benchmarkMullW(b, 16, teraAvx512Int32) }
+func BenchmarkMulInt32_24Core(b *testing.B) { benchmarkMullW(b, 24, teraAvx512Int32) }
+func BenchmarkMulInt32_32Core(b *testing.B) { benchmarkMullW(b, 32, teraAvx512Int32) }
